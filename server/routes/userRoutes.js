c351e7baf79eb378c6111cd851063fd64b60b577
samartho4 <samarthx04@gmail.com>
2025-04-01 23:07:00 -0400

    feat: Add user profile routes with JWT authentication

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,16 @@
 const express = require('express');
-const { registerUser, loginUser } = require('../controllers/userController');
+const { 
+  registerUser, 
+  loginUser, 
+  getUserProfile, 
+  updateUserProfile 
+} = require('../controllers/userController');
+const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.get('/profile', protect, getUserProfile);
+router.put('/profile', protect, updateUserProfile);
 
 module.exports = router;
